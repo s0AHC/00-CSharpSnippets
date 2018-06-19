452c871182126589a7195d565494153d32fe350d
s0AHC <zhengrong.han@outlook.com>
2018-06-19 21:50:33 +0800

    Add Snippets ProgressBar.Marquee form

---
+++ Controls/ProgressBar.MarqueeForm.cs
@@ -0,0 +1,54 @@
+//Form of ProgressBar.Marquee 
+public class WaitIndicator : IDisposable
+{
+    ProgressForm progressForm;
+    Thread thread;
+    bool disposed = false; //to avoid redundant call
+    public WaitIndicator()
+    {
+        progressForm = new ProgressForm();
+        thread = new Thread(_ => progressForm.ShowDialog());
+        thread.Start();
+    }
+ 
+    public void Dispose()
+    {
+        Dispose(true);
+    }
+ 
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            thread.Abort();
+            progressForm = null;
+        }
+        disposed = true;
+    }
+}
+ 
+class ProgressForm : Form
+{
+    public ProgressForm()
+    {
+        ControlBox = false;
+        ShowInTaskbar = false;
+        StartPosition = FormStartPosition.CenterScreen;
+        TopMost = true;
+        FormBorderStyle = FormBorderStyle.None;
+        var progreassBar = new ProgressBar() { Style = ProgressBarStyle.Marquee, 
+            Size = new System.Drawing.Size(200, 20), 
+            ForeColor = Color.Orange, MarqueeAnimationSpeed = 40 };
+        Size = progreassBar.Size;
+        Controls.Add(progreassBar);
+    }
+}
+
+
+
+//调用方法：
+
+using (new WaitIndicator())
+{
+    //code to process
+}
