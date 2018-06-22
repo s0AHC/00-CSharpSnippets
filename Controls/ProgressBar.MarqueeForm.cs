0936e7ccad3724be0b6dc670dfd18aab73c0ccbb
s0AHC <zhengrong.han@outlook.com>
2018-06-22 22:29:32 +0800

    Add reading notes

--- Controls/ProgressBar.MarqueeForm.cs
+++ Controls/ProgressBar.MarqueeForm.cs
@@ -1,4 +1,22 @@
 //Form of ProgressBar.Marquee 
+
+class ProgressForm : Form
+{
+    public ProgressForm()
+    {
+        ControlBox = false;
+        ShowInTaskbar = false;
+        StartPosition = FormStartPosition.CenterScreen;
+        TopMost = true;
+        FormBorderStyle = FormBorderStyle.None;
+        var progreassBar = new ProgressBar() { Style = ProgressBarStyle.Marquee, 
+            Size = new System.Drawing.Size(200, 20), 
+            ForeColor = Color.Orange, MarqueeAnimationSpeed = 40 };
+        Size = progreassBar.Size;
+        Controls.Add(progreassBar);
+    }
+}
+
 public class WaitIndicator : IDisposable
 {
     ProgressForm progressForm;
@@ -27,27 +45,11 @@ public class WaitIndicator : IDisposable
     }
 }
  
-class ProgressForm : Form
-{
-    public ProgressForm()
-    {
-        ControlBox = false;
-        ShowInTaskbar = false;
-        StartPosition = FormStartPosition.CenterScreen;
-        TopMost = true;
-        FormBorderStyle = FormBorderStyle.None;
-        var progreassBar = new ProgressBar() { Style = ProgressBarStyle.Marquee, 
-            Size = new System.Drawing.Size(200, 20), 
-            ForeColor = Color.Orange, MarqueeAnimationSpeed = 40 };
-        Size = progreassBar.Size;
-        Controls.Add(progreassBar);
-    }
-}
+
 
 
 
 //调用方法：
-
 using (new WaitIndicator())
 {
     //code to process
