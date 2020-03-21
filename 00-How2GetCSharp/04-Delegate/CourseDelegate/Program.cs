48536fcb241c5d562f4873f4afb6dd131ad24681
s0AHC <zhengrong.han@outlook.com>
2020-03-21 15:24:11 +0800

    Add new courses 04-Delegate and 05-Event!

---
+++ 00-How2GetCSharp/04-Delegate/CourseDelegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CourseDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
