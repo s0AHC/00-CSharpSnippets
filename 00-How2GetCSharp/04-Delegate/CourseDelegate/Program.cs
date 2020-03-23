84c91dd9caef2dcd54d22e8e181a0672e6282cf4
s0AHC <zhengrong.han@outlook.com>
2020-03-23 13:12:15 +0800

    Add snippts of customized & Generic delegate into course!

--- 00-How2GetCSharp/04-Delegate/CourseDelegate/Program.cs
+++ 00-How2GetCSharp/04-Delegate/CourseDelegate/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CourseDelegate
 {
     class Program
     {
+        // Define some customized delegates
+        // 自定义一些代理
+        public delegate int CustomizedIntDelegate(int OperandA,int OperandB);  
+        public delegate double CustomizedDblDelegate(Double OperandA,double OperandB); 
+
+        // Define a customized generic delegate
+        // 自定义一个泛型代理
+        public delegate void CustomedGenericDelegate<T>(T OperandA,T OperandB);
         static void Main(string[] args)
         {
             /*
@@ -13,10 +22,10 @@ namespace CourseDelegate
             
             //Example: Action, for subroutine. 
             //本例: Action，用于无法返回的函数
-            Action<int, int> multiplication=new Action<int, int>(MyMath.Multiplication); //Attention: just put method's name in Action invoke, don't need()
+            Action<int, int> multiplication=new Action<int, int>(MyMath.Multiplication); //Attention: just put method's name in Action invoke, suffix no more ()"
             Action<int, int> subtraction=new Action<int, int>(MyMath.Subtraction);
-            multiplication(100,200);
-            subtraction(300,50);
+            multiplication(100,200);  //invoke used, but as you see at below it can be omitted!
+            subtraction(300,50);  //invoke was omitted!
 
             //Example: Func, for function
             //本例: Func, 用于有返回值得的函数
@@ -24,6 +33,21 @@ namespace CourseDelegate
             System.Console.WriteLine($"Func delegate, add return: {add(100,32)}");;
             Func<double,double,double> divide=new Func<double,double,double>(MyMath.Divide);
             System.Console.WriteLine($"Func delegate, divide return: {divide(400,2)}");;
+
+            CustomizedIntDelegate cdAdd=new CustomizedIntDelegate(MyMath.Add);
+            var v= cdAdd(300, 500);
+            
+            System.Console.WriteLine( value: $"Customed delegate Add method: {v}");
+            CustomizedDblDelegate cdDiv=new CustomizedDblDelegate(MyMath.Divide);
+            var v1 = cdDiv(1000.00,200.00);
+            System.Console.WriteLine(value: $"Customed delegate Divide method: {v1}");
+
+            CustomedGenericDelegate<int> cgdIntMul=new CustomedGenericDelegate<int>(MyMath.Multiplication);
+            CustomedGenericDelegate<double> cgdDblSub=new CustomedGenericDelegate<double>(MyMath.DblSubtraction);
+
+            cgdIntMul(20,300);
+            cgdDblSub(999,333);
+
         }
         
         class MyMath
@@ -38,6 +62,7 @@ namespace CourseDelegate
             //功能方法
             public static void Multiplication(int OperandA, int OperandB) => Console.WriteLine($"Action delegation,Multiplication: {OperandA * OperandB}");
             public static void Subtraction(int OperandA, int OperandB) => Console.WriteLine($"Action delegation,Subtraction: {OperandA - OperandB}");
+            public static void DblSubtraction(double OperandA,double OperandB)=>System.Console.WriteLine($"Generic delegate, Double Subtraction: {OperandA-OperandB}");
         }
     }
 }
