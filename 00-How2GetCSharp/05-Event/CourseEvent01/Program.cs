017a9f972e6214311b34b4f6ca95ce98d9792426
s0AHC <zhengrong.han@outlook.com>
2020-03-24 16:10:04 +0800

    Step for code an Event?

--- 00-How2GetCSharp/05-Event/CourseEvent01/Program.cs
+++ 00-How2GetCSharp/05-Event/CourseEvent01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Timers;
 
 namespace CourseEvent01
 {
@@ -6,7 +7,25 @@ namespace CourseEvent01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1. Event source: timer is an Event source.
+            Timer timer=new Timer();  
+            timer.Interval=1500;
+            Boy boy=new Boy();
+
+            // 2. Event: Elapsed就是timer这个事件拥有者的事件
+            // 5. 事件订阅: boy.Action方法的作为事件处理器，订阅了timer的Elapsed事件。订阅符号是+=。
+            timer.Elapsed+=boy.Action;
+        }        
+    }
+
+    // 3. Event responder: event responder should has an Event handler to Subscribe the Event
+    // 事件响应者，事件响应者应该有一个方法叫事件处理器用于订阅事件
+    class Boy
+    {
+        // 4. Event handler: Action是Boy这个事件响应者的事件处理器方法
+        internal void Action(object sender, ElapsedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
