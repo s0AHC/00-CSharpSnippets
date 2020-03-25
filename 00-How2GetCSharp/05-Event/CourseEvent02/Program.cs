196b5967325a7d664fe481a13eae27f55de5f732
s0AHC <zhengrong.han@outlook.com>
2020-03-25 16:15:04 +0800

    upload unfinished customized event!

--- 00-How2GetCSharp/05-Event/CourseEvent02/Program.cs
+++ 00-How2GetCSharp/05-Event/CourseEvent02/Program.cs
@@ -2,11 +2,31 @@
 
 namespace CourseEvent02
 {
+    
+    /*
+        4. Event handler: remeber we use delegate to subscribe the Event.
+            a. In the arguments list the HereIsEventSource specified the event source, 
+            b. the EventArgs specified which information should be send to event responder 
+    */
+    public delegate void MyEventHandler(HereIsEventSource eventSource, EventNameEventArgs e);
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             
         }
+    }
+
+    // Define details arguments, this class should derived from EventArgs
+    public class EventNameEventArgs:EventArgs
+    {
+        public string FirstArgs{get;set;}
+        public string SecondArgs{get;set;}
+    }
+
+    // 1. Event source: This class define a event source
+    public class HereIsEventSource
+    {
+        
     }
 }
